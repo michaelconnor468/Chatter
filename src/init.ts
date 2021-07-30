f6bb688968b3d0c1e0e7a26403d33371b8516930
Michael Connor <michaelconnor468@gmail.com>
2021-07-30 19:08:12 -0400

    added config file

--- src/init.ts
+++ src/init.ts
@@ -1,8 +1,9 @@
-import express from "express";
-import http from "http";
-import path from "path";
+import express from 'express';
+import http from 'http';
+import path from 'path';
+import config from './config'
 
-const PORT: number = 8000;
+const PORT: number = config.port;
 const app = express();
 
 app.use(express.static('client/dist'));
