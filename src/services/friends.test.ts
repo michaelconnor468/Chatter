a04031245568b29cb6cdc13b4fbed28e77ac912c
Michael Connor <michaelconnor468@gmail.com>
2022-07-30 13:32:36 -0400

    added wrapping describe to test boilerplate

--- src/services/friends.test.ts
+++ src/services/friends.test.ts
@@ -1,5 +1,7 @@
 import friendsRouter from './friends';
 
-test('TestJestWorkingAsExpected', () => {
-    expect(2).toBe(2);  
+describe('TestFriendsGetEndpoint', () => {
+    test('TestJestWorkingAsExpected', () => {
+        expect(2).toBe(2);  
+    });
 });
